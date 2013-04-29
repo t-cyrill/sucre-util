52f6df5fe5490dada5cff7b3cc928c69953b6f8b
cyrill <siril.taka@gmail.com>
2013-04-29 16:46:54 +0900

    Rewrite LoggerSample using CLI\Application

--- sample/LoggerSample.php
+++ sample/LoggerSample.php
@@ -3,35 +3,41 @@ namespace Sucre;
 
 require dirname(__DIR__).'/vendor/autoload.php';
 
-Logger::debug('test');
-Logger::info('test');
-Logger::warn('test');
-Logger::error('test');
-Logger::fatal('test');
+class LoggerSample extends CLI\Application {
+    public function run()
+    {
+        Logger::debug('test');
+        Logger::info('test');
+        Logger::warn('test');
+        Logger::error('test');
+        Logger::fatal('test');
 
-sleep(1);
+        sleep(1);
 
-$colorize = Logger\ColorizeStandardOutLogger::factory();
-Logger::setLogger($colorize);
-Logger::debug('test');
-Logger::info('test');
-Logger::warn('test');
-Logger::error('test');
-Logger::fatal('test');
+        $colorize = Logger\ColorizeStandardOutLogger::factory();
+        Logger::setLogger($colorize);
+        Logger::debug('test');
+        Logger::info('test');
+        Logger::warn('test');
+        Logger::error('test');
+        Logger::fatal('test');
 
-sleep(1);
+        sleep(1);
 
-$std_logger = Logger\StandardOutLogger::factory();
-$std_logger->setLevel(Logger::INFO);
-$multi_logger = Logger\MultiLogger::factory();
-$multi_logger->addLogger($std_logger);
-$multi_logger->addLogger($colorize);
+        $std_logger = Logger\StandardOutLogger::factory();
+        $std_logger->setLevel(Logger::INFO);
+        $multi_logger = Logger\MultiLogger::factory();
+        $multi_logger->addLogger($std_logger);
+        $multi_logger->addLogger($colorize);
 
-Logger::setLogger($multi_logger);
-Logger::debug('test');
-Logger::info('test');
-Logger::warn('test');
-Logger::error('test');
-Logger::fatal('test');
+        Logger::setLogger($multi_logger);
+        Logger::debug('test');
+        Logger::info('test');
+        Logger::warn('test');
+        Logger::error('test');
+        Logger::fatal('test');
+    }
+}
 
+LoggerSample::factory($argv)->run();
 
