2f9278d9cd3889ee20857b150d674b49fb3518e3
cyrill <siril.taka@gmail.com>
2013-04-29 15:05:20 +0900

    Add ColorEscape

---
+++ lib/Sucre/CLI/ColorEscape.php
@@ -0,0 +1,10 @@
+<?php
+namespace Sucre\CLI;
+
+class ColorEscape {
+    const RED    = "\e[0;31m";
+    const GREEN  = "\e[0;32m";
+    const YELLOW = "\e[0;33m";
+    const NORMAL = "\e[0;39m";
+}
+
