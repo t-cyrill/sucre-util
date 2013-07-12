c10a9dec85013959628d10c5718d4ea699959627
cyrill <siril.taka@gmail.com>
2013-07-12 10:16:40 +0900

    Update CLI/Application.php

--- lib/Sucre/CLI/Application.php
+++ lib/Sucre/CLI/Application.php
@@ -10,17 +10,5 @@ use Sucre\SucreObject;
  */
 abstract class Application extends SucreObject
 {
-    private $args = array();
-
     abstract public function run();
-
-    protected function __construct(array $params = array())
-    {
-        $this->args = $params[0];
-    }
-
-    protected function getArgs()
-    {
-        return $this->args;
-    }
 }
